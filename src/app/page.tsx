5158104f7827b5ec22e1968a08a74e8cfe58d7f3
SeharFatima <160278458+SehrishAkbar@users.noreply.github.com>
2025-01-02 09:58:40 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+
+"use client";
+import Contact from "@/Components/Contact";
+import Hero from "@/Components/Hero";
+import Projects from "@/Components/Projects";
+import Skills from "@/Components/Skills";
+import About from "@/Components/About";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay: 100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <main>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <About />
+    </main>
+  );
+}
+
